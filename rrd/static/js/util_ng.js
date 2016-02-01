cb42db8bf3c6d8d513b03b330a6e124ca59803f6
Kordan Ou <kordan@hitripod.com>
2016-02-01 23:14:33 +0800

    Merge pull request #21 from chuanxd/loading_image
    Loading image

--- rrd/static/js/util_ng.js
+++ rrd/static/js/util_ng.js
@@ -53,7 +53,7 @@ function myFlot(FlotServ) {
         var legendContainer = $element.find('.legend');
         var data;
         var dataBak;
-        $scope.$watch('config', function(val) {
+        var configHandle = $scope.$watch('config', function(val) {
             if (val && val.length) {
                 // 这里的 数据需要排序, 貌似angular把这里的顺序弄乱了
                 data = FlotServ.sortData(val);
@@ -69,6 +69,7 @@ function myFlot(FlotServ) {
             }
             loadingIcon.hide();
             el.find('.loading').remove();
+            configHandle();
         });
 
         $element.parents().find('.reset-zoom').on('click', function(e) {
