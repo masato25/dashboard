2c540064863dc127c7e693839bc769cf88e42cc4
Minimum <minimum-hsu@users.noreply.github.com>
2016-08-05 11:51:55 +0800

    Remove python virtualenv

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,4 @@
 #-*- coding:utf-8 -*-
-import os
-base_dir = os.path.dirname(os.path.abspath(__file__))
-activate_this = '%s/env/bin/activate_this.py' % base_dir
-execfile(activate_this, dict(__file__=activate_this))
-
-import sys
-sys.path.insert(0, base_dir)
 
 from rrd import app
 
